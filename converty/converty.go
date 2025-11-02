90160f4969185458761eef6606ed640c1e4e35a1
Eero <eerolillemae@gmail.com>
2025-11-02 09:42:44 +0200

    fixed a bug and refactored a bit

--- converty/converty.go
+++ converty/converty.go
@@ -1,8 +1,14 @@
 package converty
 
-func Convert_to_fahrenheit(celsius_value float64) float64 {
+func Celsius_to_fahrenheit(celsius_value float64) float64 {
 	var fahrenheit_value float64
 	fahrenheit_value = celsius_value * 9 / 5 + 32
 	return fahrenheit_value
 }
 
+func Fahrenheit_to_celsius(value float64) float64 {
+	// var celsius_value float64
+	// celsius_value = (fahrenheit_value - 32) * 5 / 9
+	// return celsius_value
+	return ((value - 32) * 5 / 9)
+}
--- main.go
+++ main.go
@@ -3,12 +3,55 @@ package main
 import (
 	"fmt"
 	"github.com/myprojectsandprojects/converty/converty"
+	"os"
+	"strconv"
+	// "log"
 )
 
 func main() {
-	fmt.Printf("Enter Celsius value:\n")
-	var celsius_value float64
-	fmt.Scan(&celsius_value)
-	// fmt.Printf("celsius_value is %v\n", celsius_value)
-	fmt.Printf("fahrenheit value is %v\n", converty.Convert_to_fahrenheit(celsius_value))
+	args := os.Args[1:]
+
+	// fmt.Println(args)
+	// fmt.Println(len(args))
+
+	if (len(args) == 3) {
+		value, err := strconv.ParseFloat(args[0], 64)
+		if err != nil {
+			print_usage_and_exit()
+		}
+
+		if args[1] == "Celsius" {
+			if args[2] == "Fahrenheit" {
+				fmt.Println(converty.Celsius_to_fahrenheit(value))
+			} else {
+				print_usage_and_exit()			
+
+			}
+		} else if args[1] == "Fahrenheit" {
+			if args[2] == "Celsius" {
+				fmt.Println(converty.Fahrenheit_to_celsius(value))
+			} else {
+				print_usage_and_exit()
+			}
+		} else {
+			print_usage_and_exit()
+		}
+	} else {
+		print_usage_and_exit()
+	}
+	
+	// fmt.Printf("Enter Celsius value:\n")
+	// var celsius_value float64
+	// fmt.Scan(&celsius_value)
+	// // fmt.Printf("celsius_value is %v\n", celsius_value)
+	// fmt.Printf("fahrenheit value is %v\n", converty.Convert_to_fahrenheit(celsius_value))
+}
+
+func print_usage_and_exit() {
+	fmt.Println("Usage:\n")
+	fmt.Println("[program name] [value to be converted] [from what units] [to what units]")
+	fmt.Println("Only supported units are Celsius and Fahrenheit.")
+	fmt.Println("Value to be converted is a floating point number")
+
+	os.Exit(1)
 }
